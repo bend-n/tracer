307c87ddb3e200977d5176c3dedc8f04c1709f41
bendn <bend.n@outlook.com>
2023-04-01 20:22:35 +0700

    mouse warp when panning + fix delete button

--- ui/editor/cam.gd
+++ ui/editor/cam.gd
@@ -4,25 +4,32 @@ var freelook := false;
 var panning := false;
 var mp_before_freelook := Vector2.ZERO;
 const MOUSE_SENSITIVTY = 0.02;
-const PAN_SENSITIVITY = 0.005;
+const PAN_SENSITIVITY = 0.5;
 const SCROLL_SENS = 2;
 const CAMERA_MAX_ROTATION_ANGLE = deg_to_rad(70);
 
+var m_vel := Vector2.ZERO
+@onready var last_m_pos := get_viewport().get_mouse_position()
+
 func _process(_delta):
+	m_vel = get_viewport().get_mouse_position() - last_m_pos
+	last_m_pos = get_viewport().get_mouse_position()
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
 			mp_before_freelook = get_viewport().get_mouse_position()
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 			freelook = true;
 	elif Input.is_action_pressed("ui_pan"):
-		var vel := Input.get_last_mouse_velocity() * PAN_SENSITIVITY
+		var vel := m_vel * PAN_SENSITIVITY
 		position -= (transform.basis.x * vel.x)
 		position += (transform.basis.y * vel.y)
+		panning = true
 	else:
 		if Input.get_mouse_mode() != Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
 			freelook = false;
 			get_viewport().warp_mouse(mp_before_freelook)
+		panning = false
 
 func _input(event: InputEvent):
 	if freelook and event is InputEventMouseMotion:
@@ -33,3 +40,11 @@ func _input(event: InputEvent):
 			# camera zoom
 			MOUSE_BUTTON_WHEEL_UP: global_position -= global_transform.basis.z * SCROLL_SENS
 			MOUSE_BUTTON_WHEEL_DOWN: global_position += global_transform.basis.z * SCROLL_SENS
+	elif event is InputEventMouseMotion:
+		var v := get_viewport()
+		if not Rect2(Vector2(), v.size).grow(-15).has_point(get_viewport().get_mouse_position()):
+			if panning:
+				var mp := v.get_mouse_position()
+				v.warp_mouse(mp.posmodv(v.size))
+				last_m_pos = mp.posmodv(v.size)
+
--- ui/editor/delete.gd
+++ ui/editor/delete.gd
@@ -8,8 +8,8 @@ func _on_items_selected_node(node: Node) -> void:
 func _pressed() -> void:
 	var tobj := (owner as TrackEditor).tobj_from_node(owner.selected)
 	hist.create_action("delete")
-	hist.add_do_method(%items.remove_obj.bind(tobj, owner.selected))
+	hist.add_do_method(%view.remove_obj.bind(tobj, owner.selected))
 	hist.add_undo_reference(owner.selected)
-	hist.add_undo_method(%items.add_obj.bind(tobj, owner.selected))
+	hist.add_undo_method(%view.add_obj.bind(tobj, owner.selected))
 	hist.commit_action()
 	disabled = true
