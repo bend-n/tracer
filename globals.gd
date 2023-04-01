993d4dd3116c2839bd20607bc3e65a967c2175fa
bendn <bend.n@outlook.com>
2023-04-01 08:26:10 +0700

    snap vertically only by 5m instead of 10m

--- globals.gd
+++ globals.gd
@@ -9,3 +9,9 @@ const THUMBS := "user://thumbs/%s.thumb"
 const DEFAULT_EDITOR_LAYER := 0x80000000
 const GIZMO_LAYER := 0x40000000
 const GIZMO_LAYER_I := 31
+
+static func snap_v(x: float, y: float, z: float, v: Vector3):
+	v.x = snappedf(v.x, x)
+	v.y = snappedf(v.y, y)
+	v.z = snappedf(v.z, z)
+	return v
