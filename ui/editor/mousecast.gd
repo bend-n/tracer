0f520a1de4e1d93f67b158edba9bec17dc65d6da
bendn <bend.n@outlook.com>
2023-04-04 08:18:42 +0700

    reimplement single object selection

--- ui/editor/mousecast.gd
+++ ui/editor/mousecast.gd
@@ -18,9 +18,15 @@ func position_to(p: Vector2):
 	target_position = c.project_ray_normal(p) * depth
 
 func _physics_process(_delta: float) -> void:
+	if gizmo_just_hit || viewport_just_dropped:
+		gizmo_just_hit = false
+		viewport_just_dropped = false
+		start = null
+		drag_area.hide()
+		drag_area.size = Vector2.ZERO
+		return
 	if (
 		start == null # not already dragging
-		and not gizmo_just_hit # didnt just use gizmo
 		and Input.is_action_just_pressed("click") # just clicked
 		and Rect2(Vector2.ZERO, get_viewport().size).has_point(get_viewport().get_mouse_position()) # mouse is over viewport
 	):
@@ -29,6 +35,16 @@ func _physics_process(_delta: float) -> void:
 	elif Input.is_action_just_released("click") and start != null:
 		var selection: Array[Block] = []
 		var r := Rect2(start, Vector2.ZERO).expand(get_viewport().get_mouse_position())
+		start = null
+		drag_area.hide()
+		drag_area.size = Vector2.ZERO
+		if r.size.length_squared() < 1000:
+			position_to(r.get_center())
+			force_raycast_update()
+			if is_colliding():
+				selection.append(get_collider())
+				hit.emit(selection)
+			return
 		for obj in editor.objects:
 			var origin := obj.transform.origin
 			if c.is_position_behind(origin) || c.global_position.distance_squared_to(origin) > 5000:
@@ -37,23 +53,10 @@ func _physics_process(_delta: float) -> void:
 			if r.has_point(point):
 				selection.append(obj.live_node)
 		hit.emit(selection)
-		start = null
-		drag_area.hide()
-		drag_area.size = Vector2.ZERO
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and start != null:
 		var r := Rect2(start, Vector2.ZERO).expand(get_viewport().get_mouse_position())
 		drag_area.position = r.position
 		drag_area.size = r.size
-	if gizmo_just_hit || viewport_just_dropped:
-		gizmo_just_hit = false
-		viewport_just_dropped = false
-		return
-	force_raycast_update()
-#	if is_colliding():
-#		var n: Block = get_collider()
-#		hit.emit(n)
-#	else:
-#		miss.emit()
 
 func gizmo_clicked() -> void:
 	gizmo_just_hit = true
